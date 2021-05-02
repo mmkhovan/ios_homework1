79f664013874ee3586ba8db9241bfe7642935fce
Mikhail Khovanskiy <mmkhovan@icloud.com>
2021-05-02 14:48:33 +0300

    Homework 6

--- homework/main.swift
+++ homework/main.swift
@@ -1,151 +1,77 @@
 //
-//  homework5.swift
+//  homework6.swift
 //  cmd_tool
 //
-//  Created by Хованский Михаил on 22.04.2021.
+//  Created by Хованский Михаил on 25.04.2021.
 //
 
 import Foundation
 
-enum EngineState2: String {
-    case on = "заведен", off = "заглушен"
+enum Wire: String {
+    case phase = "фаза"
+    case zero = "ноль"
 }
 
-enum DoorState2: String {
-    case open = "открыты", closed = "закрыты"
-}
+class WireProperties {
+    let material: Wire
+    let section: String
+    let length: Int
+    let color: String
 
-enum TrunkState: String {
-    case full = "полная", empty = "половина"
-}
-
-enum RoofState: String {
-    case full = "установлена", empty = "убрана", half = "заполнена наполовину"
-}
-
-enum WindowState: String {
-    case open = "открыты", close = "закрыты"
-}
-
-
-protocol Car3: class, CustomStringConvertible {
-    var mark: String {get}
-    var year: Int {get}
-    var km: Double{get set}
-    var vehicleWeight: Double {get set}
-    var engineState: EngineState2 { get set }
-    var windowsState: WindowState {get set}
-    var doorState: DoorState2 {get set}
-    var roofState: RoofState {get set}
-    var trunkState: TrunkState {get set}
-
-
-    func changeWindowsState(to: WindowState)
-    func changeEngineState(to: EngineState2)
-    func changeDoorState(to: DoorState2)
-}
-
-extension Car3 {
-    func changeWindowsState(to: WindowState) {
-        windowsState = to
-    }
-
-    func changeEngineState(to: EngineState2) {
-        engineState = to
-    }
-
-    func changeDoorState(to: DoorState2) {
-        doorState = to
-    }
-
-    var desription: String {
-        return "\(mark), \(year), \(km); \(vehicleWeight), \(engineState), \(windowsState),\(doorState)"
+    init(material: Wire, section: String, length: Int, color: String) {
+        self.material = material
+        self.section = section
+        self.length = length
+        self.color = color
     }
 }
 
-
-class SportCars: Car3 {
-    var mark: String
-    var year: Int
-    var km: Double
-    var vehicleWeight: Double
-    var engineState: EngineState2
-    var windowsState: WindowState
-    var doorState: DoorState2
-    var roofState: RoofState
-
-    init(mark: String, year: Int, km: Double, vehicleWeight: Double,
-         engineState: EngineState2,
-         windowsState: WindowState, doorState: DoorState2, roofState: RoofState ) {
-        self.mark = mark
-        self.year = year
-        self.km = km
-        self.vehicleWeight = vehicleWeight
-        self.engineState = engineState
-        self.windowsState = windowsState
-        self.doorState = doorState
-        self.roofState = roofState
-    }
-    func changeRoofState(to: RoofState){
-        roofState = to
+extension WireProperties {
+    var txt: String {
+        """
+            \(material.rawValue) .  \(section) . \(length) . \(color)
+        """
     }
 }
 
+struct Queue<T> {
+    private var box: [T] = []
 
-class TrunkCars: Car3 {
-    var mark: String
-    var year: Int
-    var km: Double
-    var vehicleWeight: Double
-    var engineState: EngineState2
-    var windowsState: WindowState
-    var doorState: DoorState2
-    var trunkState: TrunkState
-
-    func changeYear(to newYear: Int){
-        self.year = newYear
+    func filter(predicate: (T) -> Bool) -> [T] {
+        var tempArr: [T] = []
+        for i in box {
+            if predicate(i) {
+                tempArr.append(i)
+            }
+        }
+        return tempArr
     }
 
-    init(mark: String, year: Int, km: Double, vehicleWeight: Double, engineState: EngineState2, windowsState: WindowState, doorState: DoorState2, trunkState: TrunkState ) {
-        self.mark = mark
-        self.year = year
-        self.km = km
-        self.vehicleWeight = vehicleWeight
-        self.engineState = engineState
-        self.windowsState = windowsState
-        self.doorState = doorState
-        self.trunkState = trunkState
+    mutating func pop() -> T? {
+        guard  box.count > 0
+                else {
+            return nil
+        }
+        return box.removeFirst()
     }
 
-    func  changeTrunkState(to: TrunkState) {
-        trunkState = to
-    }
-
-}
-
-
-extension SportCars {
-    var description: String {
-        return "\(mark), \(year), \(km); \(vehicleWeight), \(engineState), \(windowsState),\(doorState), \(roofState.rawValue)"
+    mutating func push(_ i:T) {
+        box.append(i)
     }
 }
 
-extension TrunkCars {
-    var description: String {
-        return "\(mark), \(year), \(km); \(vehicleWeight), \(engineState), \(windowsState),\(doorState), \(trunkState.rawValue)"
-
+extension Queue {
+    subscript(index: Int) -> T? {
+        guard index >= 0 && index < box.count else {
+            return nil
+        }
+        return box[index]
     }
 }
 
+var BoxOfWires = Queue<WireProperties>()
 
-
-var sportcar34 = SportCars(mark: "Volvo", year: 2014, km: 10.12, vehicleWeight: 10.14, engineState: .off, windowsState: .open, doorState: .closed, roofState: .empty)
-
-var sportcar56 = SportCars(mark: "Volvo", year: 2014, km: 10.12, vehicleWeight: 10.14, engineState: .off, windowsState: .open, doorState: .closed, roofState: .full)
-
-var trunkcar34 = TrunkCars(mark: "ZIL", year: 1965, km: 10.12, vehicleWeight: 10.65, engineState: .off, windowsState: .close, doorState: .closed, trunkState: .full)
-
-var trunkcar56 = TrunkCars(mark: "ZIL", year: 1965, km: 10.12, vehicleWeight: 10.65, engineState: .off, windowsState: .close, doorState: .closed, trunkState: .empty)
-
-trunkcar34.changeYear(to: 2021)
-trunkcar34.engineState(to: .on)
+BoxOfWires.push(WireProperties(material: .phase, section: "0.5", length: 2, color: "blue"))
+BoxOfWires.push(WireProperties(material: .zero, section: "0.3", length: 4, color: "red"))
+BoxOfWires.push(WireProperties(material: .zero, section: "0.5", length: 4, color: "green"))
+BoxOfWires.push(WireProperties(material: .phase, section: "0.4", length: 5, color: "green-yellow"))
