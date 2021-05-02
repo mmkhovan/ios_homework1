d2d0f0cf7d745a352faed07e1ebed527d6493a42
Mikhail Khovanskiy <mmkhovan@icloud.com>
2021-05-02 13:39:12 +0300

    Homework 3

--- homework/main.swift
+++ homework/main.swift
@@ -1,71 +1,108 @@
 //
-//  homework2.swift
+//  homework4.swift
 //  cmd_tool
 //
-//  Created by Хованский Михаил on 23.04.2021.
+//  Created by Хованский Михаил on 22.04.2021.
 //
 
 import Foundation
 
-/// Task 1
-func isEvenNumber(number: Int) -> Bool { number % 2 == 0 }
 
-
-/// Task 2
-func isNumber(_ number: Int, dividedBy divider: Int) -> Bool {
-    return number % divider == 0
+enum Transmission {
+    case auto, manual
 }
 
-/// Task 3
-class SomeArray {
-    init() {
-        var Arr = [Int]()
+enum EngineState {
+    case on, off
+}
 
-        for i in 1...100 {
-            Arr.append(i)
-        }
+enum DoorState {
+    case open, closed
+}
 
-        var otherArray = [1...100]
+enum TankLevel {
+    case full, empty
+}
 
-        for item in Arr {
-            if isEvenNumber(number: item) || !isNumber(item, dividedBy: 3) {
-                if let index = Arr.firstIndex(of: item) {
-                    Arr.remove(at: index)
-                }
-            }
-        }
+enum Trailer {
+    case big, small
+}
 
-        print(Arr)
+struct TrunkCar {
+    var brand: String
+    var Trailer: Trailer
+    var TrunkCapacity: Int
+    var TrunkFilledCapacity: Int
+    var engineCapacity: Int
+    var spareWheel: Bool
+    var engineState: EngineState
+    var doorState: DoorState
+    var tankLevel: TankLevel
 
-        func filterNumbers(number: Int) -> Bool {
-            return number % 2 != 0 && number % 3 == 0
-        }
+    mutating func openDoor() {
+        doorState = .open
+    }
 
-        var newArray = Array(1...100).filter { $0 % 2 != 0 && $0 % 3 == 0}
+    mutating func closeDoor() {
+        doorState = .closed
+    }
+
+    mutating func tankLevel(_ tankLevel: TankLevel) {
+        self.tankLevel = tankLevel
+    }
+
+    mutating func engineState(_ engineState: EngineState) {
+        self.engineState = engineState
     }
 }
 
 
-/// Task 5
-class Fibb {
-    init() {
-        typealias Fibonaccitype = Decimal
+struct SportCar {
+    var brand: String
+    var yearOfIssue: Int
+    var maxSpeed: Int
+    var transmission: Transmission
+    var parkingSensor: Bool
+    var engineState: EngineState
+    var doorState: DoorState
+    var tankLevel: TankLevel
 
-        var fibonacci = [Fibonaccitype]()
-
-        func createNextFibonacciNumber(for array: inout [Fibonaccitype]) {
-            switch array.count {
-            case 0,1:
-                array.append(1)
-            default:
-                let penultNumber = array[array.count - 2]
-                let lastNumber = array[array.count - 1]
-                array.append(penultNumber + lastNumber)
-            }
-        }
-
-        for _ in 1...100 {
-            createNextFibonacciNumber(for: &fibonacci)
-        }
+    mutating func doorState(_ doorState: DoorState) {
+        self.doorState = doorState
     }
+
+    mutating func fillTank() {
+        tankLevel = .full
+    }
+
+    mutating func emptyTank() {
+        tankLevel = .empty
+    }
+
+    mutating func engineOn() {
+        engineState = .on
+    }
+
+    mutating func engineoff() {
+        engineState = .off
+    }
+
+}
+
+var sportCar1 = SportCar(brand: "ferarri", yearOfIssue: 2001, maxSpeed: 370, transmission: .manual, parkingSensor: true, engineState: .on, doorState: .open, tankLevel: .full)
+
+var sportCar2 = SportCar(brand: "porsche", yearOfIssue: 2012, maxSpeed: 350, transmission: .manual, parkingSensor: true, engineState: .on, doorState: .open, tankLevel: .empty)
+
+var trunkCar1 = TrunkCar(brand: "GAZ", Trailer: .big, TrunkCapacity: 500, TrunkFilledCapacity: 250, engineCapacity: 2, spareWheel: true, engineState: .on, doorState: .closed, tankLevel: .full)
+
+var trunkCar2 = TrunkCar(brand: "Volvo", Trailer: .big, TrunkCapacity: 570, TrunkFilledCapacity: 300, engineCapacity: 2, spareWheel: true, engineState: .off, doorState: .closed, tankLevel: .empty)
+
+
+
+func PrintSportCarProperties(car: SportCar) {
+    print(car.brand, car.yearOfIssue, car.maxSpeed, car.transmission, car.parkingSensor, car.engineState, car.doorState, car.tankLevel)
+}
+
+func PrinttrunkCarProperties(car: TrunkCar) {
+    print(car.brand, car.Trailer, car.TrunkCapacity, car.TrunkFilledCapacity, car.engineCapacity, car.spareWheel, car.engineState, car.doorState, car.tankLevel)
 }
