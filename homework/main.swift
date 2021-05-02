1eb964e2d9ed28712687d8dbdeaec7d1dd492711
Mikhail Khovanskiy <mmkhovan@icloud.com>
2021-05-02 13:36:31 +0300

    Homework 2

--- homework/main.swift
+++ homework/main.swift
@@ -2,54 +2,70 @@
 //  homework2.swift
 //  cmd_tool
 //
-//  Created by Хованский Михаил on 22.04.2021.
+//  Created by Хованский Михаил on 23.04.2021.
 //
 
 import Foundation
 
-
-          let a = -1.0, b = -5.0, c = 6.0
-
-          var x1 = 0.0
-          var x2 = 0.0
-          let discriminant = pow(b, 2) - 4 * a * c
-
-    
-          if discriminant > 0 {
-             x1 = (-b + sqrt(discriminant)) / 2 * a
-             x2 = (-b - sqrt(discriminant)) / 2 * a
-          } else if discriminant == 0 {
-             x1 = -b / (2 * a)
-             print("x1 = \(x1)\nx2 = \(x2)")
-      }
+/// Task 1
+func isEvenNumber(number: Int) -> Bool { number % 2 == 0 }
 
 
+/// Task 2
+func isNumber(_ number: Int, dividedBy divider: Int) -> Bool {
+    return number % divider == 0
+}
 
-       let sideA = 3.0, sideB = 4.0
-       let sideC = sqrt(pow(sideA, 2) + sideB * sideB)
-       let perimeter = sideA + sideB + sideC
-       let square = (sideA + sideB) / 2
+/// Task 3
+class SomeArray {
+    init() {
+        var Arr = [Int]()
 
-       print(sideC)
-       print(perimeter)
-       print(square)
+        for i in 1...100 {
+            Arr.append(i)
+        }
+
+        var otherArray = [1...100]
+
+        for item in Arr {
+            if isEvenNumber(number: item) || !isNumber(item, dividedBy: 3) {
+                if let index = Arr.firstIndex(of: item) {
+                    Arr.remove(at: index)
+                }
+            }
+        }
+
+        print(Arr)
+
+        func filterNumbers(number: Int) -> Bool {
+            return number % 2 != 0 && number % 3 == 0
+        }
+
+        var newArray = Array(1...100).filter { $0 % 2 != 0 && $0 % 3 == 0}
+    }
+}
 
 
+/// Task 5
+class Fibb {
+    init() {
+        typealias Fibonaccitype = Decimal
 
-       var deposit = 10_000.0
-       let percent = 10.0
-       let years = 5
+        var fibonacci = [Fibonaccitype]()
 
-       let rate = percent / 100
+        func createNextFibonacciNumber(for array: inout [Fibonaccitype]) {
+            switch array.count {
+            case 0,1:
+                array.append(1)
+            default:
+                let penultNumber = array[array.count - 2]
+                let lastNumber = array[array.count - 1]
+                array.append(penultNumber + lastNumber)
+            }
+        }
 
-
-       for _ in 1...years {
-         deposit = deposit * (1 + rate)
-       }
-
-       deposit = deposit * pow(1 + rate, Double(years))
-        
-       "Deposit = \(round(deposit * 100) / 100) rub"
-        
-       String(format: "%.2f", deposit)
-
+        for _ in 1...100 {
+            createNextFibonacciNumber(for: &fibonacci)
+        }
+    }
+}
